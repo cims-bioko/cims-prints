159be1ac581ef74cfd30d732dcd788f23bb8fe44
Brent Atkinson <brent.atkinson@gmail.com>
2018-01-07 10:27:23 +0100

    Removed vestiges of removed crash reporting functionality

--- app/src/main/java/com/openandid/core/CommCareSyncService.java
+++ app/src/main/java/com/openandid/core/CommCareSyncService.java
@@ -97,13 +97,6 @@ public class CommCareSyncService extends Service {
                     PendingIntent.getService(this, 0, killIntent, 0));
         }
 
-        if (sharedPref.getBoolean("acra.verbose", false)) {
-            Intent crashIntent = new Intent(this, NotificationReceiver.class);
-            crashIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-            crashIntent.setAction("CRASH");
-            builder.addAction(android.R.drawable.ic_delete, "Send Error Report",
-                    PendingIntent.getService(this, 0, crashIntent, 0));
-        }
         return builder.build();
     }
 
